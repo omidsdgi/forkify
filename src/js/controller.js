2494eea42fcdec7d9cef709045444013896587c5
omidsdgi <omid69sdgi@gmail.com>
2026-07-24 13:19:12 +0330

    feat: add image loading spinner

--- src/js/controller.js
+++ src/js/controller.js
@@ -15,9 +15,22 @@ const timeout = function (s) {
 // https://forkify-api.jonas.io
 
 ///////////////////////////////////////
+const renderSpinner=function(parentEl){
+  const markup=`
+  <div class="spinner">
+          <svg>
+            <use href="${icons}#icon-loader"></use>
+          </svg>
+        </div>
+  `
+  parentEl.innerHTML = '';
+  parentEl.insertAdjacentHTML('afterbegin', markup);
+}
+
 const showRecipe=async function () {
   try {
     //loading recipe
+    renderSpinner(recipeContainer);
     const res=await fetch(
       'https://forkify-api.jonas.io/api/v2/recipes/664c8f193e7aa067e94e8823'
     )
