8f64d2d92ab40781d82004571fc74e57b70f147c
omidsdgi <omid69sdgi@gmail.com>
2026-08-08 19:28:16 +0330

    feat(controller): implement pub-sub pattern

--- src/js/controller.js
+++ src/js/controller.js
@@ -28,8 +28,11 @@ const controlRecipes=async function () {
   //Rendering recipe
     recipeView.render(model.state.recipe);
 }catch (err) {
-  alert(err)
+    console.error(err)
 }
 }
-const events=['hashchange', 'load'];
-events.forEach(ev=>window.addEventListener(ev,controlRecipes))
+
+const init=function(){
+  recipeView.addHandlerRender(controlRecipes);
+}
+init()
