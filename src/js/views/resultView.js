0791d9e0bc29739063cafd3c91c1f869e9b5df3a
omidsdgi <omid69sdgi@gmail.com>
2026-08-12 14:21:49 +0330

    feat(renderError):render recipe  not found  error

--- src/js/views/resultView.js
+++ src/js/views/resultView.js
@@ -3,6 +3,8 @@ import View from './view';
 
 class ResultView extends View{
   _parentElement=document.querySelector('.results');
+  _errorMessage='No recipe found for your query! Please try again. ';
+  _message=''
 
   _generateMarkup(){
     return this._data.map(this._generateMarkupPreview).join('');
