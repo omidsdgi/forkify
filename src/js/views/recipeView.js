e09f57d225dfe8648d4e56e839cd02777f0d6311
omidsdgi <omid69sdgi@gmail.com>
2026-08-12 14:09:48 +0330

    refactor(view): update view

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -53,10 +53,7 @@ class RecipeView  extends View {
             </div>
           </div>
 
-          <div class="recipe__user-generated">
-            <svg>
-              <use href="${icons}#icon-user"></use>
-            </svg>
+          <div class="recipe__user-generated">           
           </div>
           <button class="btn--round">
             <svg class="">
