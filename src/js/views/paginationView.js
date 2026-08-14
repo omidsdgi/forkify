19db8507b7b0a2ea13d47b6169f5bb32dda30834
omidsdgi <omid69sdgi@gmail.com>
2026-08-14 17:38:58 +0330

    feat(pagination view): compute  total pagination pages

--- src/js/views/paginationView.js
+++ src/js/views/paginationView.js
@@ -6,7 +6,8 @@ class PaginationView extends View{
   _parentElement=document.querySelector('.pagination');
 
   _generateMarkup(){
-
+    const numPages= Math.ceil(this._data.results.length / this._data.resultsPerPage)
+    console.log(numPages);
   }
 }
 
