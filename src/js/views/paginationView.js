c384d7cb171316d507891fb570951150c951724d
omidsdgi <omid69sdgi@gmail.com>
2026-08-14 17:55:40 +0330

    feat(pagination view): implement   pagination button condition

--- src/js/views/paginationView.js
+++ src/js/views/paginationView.js
@@ -1,13 +1,32 @@
 import View from './view';
 import icons from 'url:../../img/icons.svg';
-import RecipeView from './recipeView';
 
 class PaginationView extends View{
   _parentElement=document.querySelector('.pagination');
 
   _generateMarkup(){
-    const numPages= Math.ceil(this._data.results.length / this._data.resultsPerPage)
+    const numPages= Math.ceil(
+      this._data.results.length / this._data.resultsPerPage
+    )
     console.log(numPages);
+
+    // Page 1, and there are other pages
+    if (this._data.page === 1 && numPages>1){
+      return 'page 1, others'
+    }
+
+    // Last page
+    if (this._data.page === numPages && numPages>1){
+      return 'Last page'
+    }
+
+    // Other page
+    if (this._data.page < numPages){
+      return 'other page'
+    }
+
+    //Page 1, and there are No other pages
+    return 'only 1 page'
   }
 }
 
