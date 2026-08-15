5d55deec7c11cfb1953cdd8620ad688a2d108e9f
omidsdgi <omid69sdgi@gmail.com>
2026-08-15 13:37:09 +0330

    feat(pagination view): implement  _generateButton  with data-goto

--- src/js/views/paginationView.js
+++ src/js/views/paginationView.js
@@ -17,43 +17,49 @@ class PaginationView extends View{
     const numPages= Math.ceil(
       this._data.results.length / this._data.resultsPerPage
     )
-    const prevButton=`
-     <button class="btn--inline pagination__btn--prev">
+
+
+    // Page 1, and there are other pages
+    if (curPage === 1 && numPages>1){
+      return this._generateNextButton(curPage)
+    }
+
+    // Last page
+    if (curPage === numPages && numPages>1){
+      return this._generatePrevButton(curPage)
+      }
+
+    // Other page
+    if (curPage < numPages){
+      return `
+      ${this._generatePrevButton(curPage)}
+      ${this._generateNextButton(curPage)}
+      `
+    }
+
+    //Page 1, and there are No other pages
+    return ''
+  }
+  _generatePrevButton(curPage) {
+    return `
+     <button data-goto="${curPage - 1}" class="btn--inline pagination__btn--prev">
             <svg class="search__icon">
               <use href="${icons}#icon-arrow-left"></use>
             </svg>
-            <span> Page ${curPage-1} </span>
+            <span> Page ${curPage - 1} </span>
           </button>
     `
-    const nextButton=`
-    <button class="btn--inline pagination__btn--next">
+  }
+  _generateNextButton(curPage) {
+    return `
+    <button data-goto="${curPage + 1}" class="btn--inline pagination__btn--next">
             <span>Page ${curPage + 1}</span>
             <svg class="search__icon">
               <use href="${icons}#icon-arrow-right"></use>
             </svg>
           </button>
     `
-    console.log(curPage,numPages);
-
-    // Page 1, and there are other pages
-    if (curPage === 1 && numPages>1){
-      return `${nextButton}
-      `
-    }
-
-    // Last page
-    if (curPage === numPages && numPages>1){
-      return `${prevButton}`
-      }
-
-    // Other page
-    if (curPage < numPages){
-      return `${prevButton} ${nextButton}`
-    }
-
-    //Page 1, and there are No other pages
-    return ''
   }
-}
+  }
 
 export default new PaginationView()
