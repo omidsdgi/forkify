5bf8110f1591abc9c5e867ac830ceb6d6adb190e
omidsdgi <omid69sdgi@gmail.com>
2026-08-15 12:57:02 +0330

    refactor(pagination view): extract reusable prevButton and nextButton

--- src/js/views/paginationView.js
+++ src/js/views/paginationView.js
@@ -9,48 +9,38 @@ class PaginationView extends View{
     const numPages= Math.ceil(
       this._data.results.length / this._data.resultsPerPage
     )
-    console.log(curPage,numPages);
-
-    // Page 1, and there are other pages
-    if (curPage === 1 && numPages>1){
-      return `
-      <button class="btn--inline pagination__btn--next">
+    const prevButton=`
+     <button class="btn--inline pagination__btn--prev">
+            <svg class="search__icon">
+              <use href="${icons}#icon-arrow-left"></use>
+            </svg>
+            <span> Page ${curPage-1} </span>
+          </button>
+    `
+    const nextButton=`
+    <button class="btn--inline pagination__btn--next">
             <span>Page ${curPage + 1}</span>
             <svg class="search__icon">
               <use href="${icons}#icon-arrow-right"></use>
             </svg>
           </button>
+    `
+    console.log(curPage,numPages);
+
+    // Page 1, and there are other pages
+    if (curPage === 1 && numPages>1){
+      return `${nextButton}
       `
     }
 
     // Last page
     if (curPage === numPages && numPages>1){
-      return `
-      <button class="btn--inline pagination__btn--prev">
-            <svg class="search__icon">
-              <use href="${icons}#icon-arrow-left"></use>
-            </svg>
-            <span> Page ${curPage-1} </span>
-          </button>
-      `
-    }
+      return `${prevButton}`
+      }
 
     // Other page
     if (curPage < numPages){
-      return `
-      <button class="btn--inline pagination__btn--prev">
-            <svg class="search__icon">
-              <use href="${icons}#icon-arrow-left"></use>
-            </svg>
-            <span> Page ${curPage-1} </span>
-          </button>
-          <button class="btn--inline pagination__btn--next">
-            <span>Page ${curPage + 1}</span>
-            <svg class="search__icon">
-              <use href="${icons}#icon-arrow-right"></use>
-            </svg>
-          </button>
-      `
+      return `${prevButton} ${nextButton}`
     }
 
     //Page 1, and there are No other pages
