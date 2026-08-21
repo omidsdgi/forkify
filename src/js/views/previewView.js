9218f1a52fad55e7c11cf2268e6430cc5024d01a
omidsdgi <omid69sdgi@gmail.com>
2026-08-21 18:30:14 +0330

    feat(resultView): render resultView with preview View

--- src/js/views/previewView.js
+++ src/js/views/previewView.js
@@ -13,11 +13,11 @@ class previewView extends View{
                 this._data.id === id ? 'preview__link--active' : ''
               }" href="#${this._data.id}">
               <figure class="preview__fig">
-                <img src="${result.image}" alt="${result.title}" />
+                <img src="${this._data.image}" alt="${this._data.title}" />
               </figure>
               <div class="preview__data">
-                <h4 class="preview__title">${result.title}</h4>
-                <p class="preview__publisher">${result.publisher}</p>                
+                <h4 class="preview__title">${this._data.title}</h4>
+                <p class="preview__publisher">${this._data.publisher}</p>                
               </div>
             </a>
           </li>
--- src/js/views/resultView.js
+++ src/js/views/resultView.js
@@ -1,5 +1,6 @@
-import icons from 'url:../../img/icons.svg';
 import View from './view';
+import previewView from './previewView';
+import icons from 'url:../../img/icons.svg';
 
 class ResultView extends View{
   _parentElement=document.querySelector('.results');
@@ -7,26 +8,11 @@ class ResultView extends View{
   _message=''
 
   _generateMarkup(){
-    return this._data.map(this._generateMarkupPreview).join('');
-
+    console.log(this._data)
+    return this._data
+      .map(result => previewView.render(result, false))
+      .join('');
   }
-  _generateMarkupPreview(result){
-    const id =window.location.hash.slice(1)
-
-    return `
-    <li class="preview">
-            <a class="preview__link ${result.id === id ? 'preview__link--active' : ''}" href="#${result.id}">
-              <figure class="preview__fig">
-                <img src="${result.image}" alt="${result.title}" />
-              </figure>
-              <div class="preview__data">
-                <h4 class="preview__title">${result.title}</h4>
-                <p class="preview__publisher">${result.publisher}</p>                
-              </div>
-            </a>
-          </li>
-    `
   }
-}
 
 export default new ResultView();
