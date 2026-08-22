194741ace07ff0c2a3717573ce920158473f941a
omidsdgi <omid69sdgi@gmail.com>
2026-08-22 20:26:54 +0330

    fix(bookmarks): fix bookmarks rendering after localStorage initialization

--- src/js/controller.js
+++ src/js/controller.js
@@ -20,15 +20,18 @@ const controlRecipes=async function () {
     if (!id) return;
     recipeView.renderSpinner();
 
-    //Update results view to mark selected search result
+    // 0)Update results view to mark selected search result
     resultView.update(model.getSearchResultsPage())
+
+    // 1)Updating bookmarks view
     bookmarksView.update(model.state.bookmarks)
 
-    //loading recipe
+    // 2)loading recipe
     await model.loadRecipe(id)
 
-  //Rendering recipe
+  // 3)Rendering recipe
     recipeView.render(model.state.recipe);
+
    }catch (err) {
     recipeView.renderError();
 }
@@ -74,6 +77,7 @@ const controlServings=function(newServings){
   // recipeView.render(model.state.recipe );
   recipeView.update(model.state.recipe );
 }
+
 const controlAddBookmark=  function () {
   // 1) Add/ remove bookmark
  if (!model.state.recipe.bookmarked) model.addBookmark(model.state.recipe)
@@ -85,7 +89,12 @@ const controlAddBookmark=  function () {
   bookmarksView.render(model.state.bookmarks);
 }
 
+const controlBookmarks = function(){
+  bookmarksView.render(model.state.bookmarks);
+}
+
 const init=function(){
+  bookmarksView.addHandlerRender(controlBookmarks)
   recipeView.addHandlerRender(controlRecipes);
   recipeView.addHandlerUpdateServings(controlServings);
   recipeView.addHandlerAddBookmark(controlAddBookmark);
