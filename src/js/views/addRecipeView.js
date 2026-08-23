207a92e827b647ad58844294715a3d56145482c3
omidsdgi <omid69sdgi@gmail.com>
2026-08-23 21:20:04 +0330

    feat(add recipe view):  hide recipe ingredient form

--- src/js/views/addRecipeView.js
+++ src/js/views/addRecipeView.js
@@ -7,19 +7,26 @@ class AddRecipeView extends View{
   _window=document.querySelector('.add-recipe-window');
   _overlay= document.querySelector('.overlay');
   _btnOpen=document.querySelector('.nav__btn--add-recipe');
-  _btnClose=document.querySelector('.btn--close-model');
+  _btnClose=document.querySelector('.btn--close-modal');
 
   constructor() {
     super();
-    this._addHandlerShowWindow()
+    this._addHandlerShowWindow();
+    this._addHandlerHideWindow();
   }
 
   toggleWindow(){
     this._overlay.classList.toggle('hidden');
     this._window.classList.toggle('hidden');
   }
+
   _addHandlerShowWindow (){
     this._btnOpen.addEventListener('click', this.toggleWindow.bind(this));
+  }
+
+  _addHandlerHideWindow (){
+    this._btnClose.addEventListener('click', this.toggleWindow.bind(this));
+    this._overlay.addEventListener('click', this.toggleWindow.bind(this));
   }
 
   _generateMarkup(){
