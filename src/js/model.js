d05feb6e480169d9d3d138326e460205de2b059f
omidsdgi <omid69sdgi@gmail.com>
2026-08-24 18:53:40 +0330

    feat(model): convert ingredient quantity to number or null

--- src/js/model.js
+++ src/js/model.js
@@ -122,8 +122,10 @@ export const uploadRecipe= async function(newRecipe){
   const ingredients = Object.entries(newRecipe).filter(
     entry => entry[0].startsWith('ingredient') && entry[1] !== '')
     .map( ing=>{
-      const [quantity, unit, description]=ing[1].replaceAll(' ', '').split(',')
-      return {quantity, unit, description}
+      const [quantity, unit, description]=ing[1]
+        .replaceAll(' ', '')
+        .split(',')
+      return {quantity: quantity ? +quantity : null, unit, description}
     })
   console.log(ingredients);
 }
