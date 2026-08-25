72787fa94539ce7d6c25d050e40648e13f7b70d1
omidsdgi <omid69sdgi@gmail.com>
2026-08-25 13:24:37 +0330

    feat(model): create recipe object to be uploaded

--- src/js/model.js
+++ src/js/model.js
@@ -123,14 +123,29 @@ export const uploadRecipe= async function(newRecipe){
     entry => entry[0].startsWith('ingredient') && entry[1] !== '')
     .map( ing=>{
       const ingArr = ing[1].replaceAll(' ', '').split(',')
-      if (ingArr.length !== 3) throw new Error( 'Wrong ingredient format! Please use the correct format:)' )
+      if (ingArr.length !== 3)
+        throw new Error(
+        'Wrong ingredient format! Please use the correct format:)'
+      )
 
       const [quantity, unit, description]=ingArr
 
       return {quantity: quantity ? +quantity : null, unit, description}
     })
-  console.log(ingredients);
+
+    const recipe= {
+      title: newRecipe.title,
+      source_url: newRecipe.sourceUrl,
+      image_url: newRecipe.image,
+      publisher: newRecipe.publisher,
+      cooking_time: +newRecipe.cookingTime,
+      servings: +newRecipe.servings,
+      ingredients:newRecipe.ingredients,
+    }
+
+  console.log(recipe);
 }catch (err){
   throw err
 }
+
 }
