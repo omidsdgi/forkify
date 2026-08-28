b5b8d3fdd7de9007bd8ced195f94f44ae60bd520
omidsdgi <omid69sdgi@gmail.com>
2026-08-28 12:48:44 +0330

    refactor(model): attach new recipe key to all API queries

--- src/js/model.js
+++ src/js/model.js
@@ -30,7 +30,7 @@ const createRecipeObject = function(data){
 
 export const loadRecipe=async function(id)  {
   try {
-  const data= await AJAX(`${API_URL}/${id}`)
+  const data= await AJAX(`${API_URL}/${id}?key=${KEY}`);
 state.recipe = createRecipeObject(data)
 
 
@@ -49,7 +49,7 @@ export const loadSearchResults= async function(query){
   try{
     state.search.query = query;
 
-    const data= await AJAX(`${API_URL}/?search=${query}`)
+    const data= await AJAX(`${API_URL}/?search=${query}&key=${KEY}`)
 
     state.search.results = data.data.recipes.map(rec=>{
       return {
