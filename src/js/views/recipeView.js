631387068b450ac9848c77168be673122af987ed
omidsdgi <omid69sdgi@gmail.com>
2026-08-28 13:00:42 +0330

    refactor(recipe-view): show generated user icon

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -71,7 +71,10 @@ class RecipeView  extends View {
             </div>
           </div>
 
-          <div class="recipe__user-generated">           
+         <div class="recipe__user-generated ${this._data.key ? '' : 'hidden'}">
+            <svg>
+              <use href="${icons}#icon-user"></use>
+            </svg>
           </div>
           <button class="btn--round btn--bookmark">
             <svg class="">
@@ -79,7 +82,8 @@ class RecipeView  extends View {
             </svg>
           </button>
         </div>
-
+        
+        
         <div class="recipe__ingredients">
           <h2 class="heading--2">Recipe ingredients</h2>
           <ul class="recipe__ingredient-list">
