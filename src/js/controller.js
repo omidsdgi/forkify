cf3b1f4119acbb3e6bafccfd7b5a5747c1014230
omidsdgi <omid69sdgi@gmail.com>
2026-08-31 12:51:27 +0330

    feat(controller): render ingredients  item on UI

--- src/js/controller.js
+++ src/js/controller.js
@@ -16,8 +16,8 @@ const showRecipe= async function(){
   try {
     // 1) Loading recipe
     const res = await fetch(
-      'https://forkify-api.jonas.io/api/v2/recipes/5ed6604591c37cdc054bc886'
       // 'https://forkify-api.jonas.io/api/v2/recipes/5ed6604591c37cdc054bc886'
+      'https://forkify-api.jonas.io/api/v2/recipes/664c8f193e7aa067e94e8297'
     );
     const data = await res.json();
 
@@ -86,30 +86,28 @@ const showRecipe= async function(){
       </button>
     </div>
 
+    
+    
+    
+    
     <div class="recipe__ingredients">
       <h2 class="heading--2">Recipe ingredients</h2>
       <ul class="recipe__ingredient-list">
-        <li class="recipe__ingredient">
-          <svg class="recipe__icon">
-            <use href="src/img/icons.svg#icon-check"></use>
-          </svg>
-          <div class="recipe__quantity">${recipe.quantity} <div class="recipe__description">
-            <span class="recipe__unit">g</span>
-            pasta
-          </div>
+      ${recipe.ingredients.map(ing=>{
+        return `
+         <li class="recipe__ingredient">
+            <svg class="recipe__icon">
+              <use href="src/img/icons.svg#icon-check"></use>
+            </svg>
+            <div class="recipe__quantity">${ing.quantity} </div>
+             <div class="recipe__description">
+              <span class="recipe__unit">${ing.unit}</span>
+              ${ing.description}
+            </div>
         </li>
-
-        <li class="recipe__ingredient">
-          <svg class="recipe__icon">
-            <use href="src/img/icons.svg#icon-check"></use>
-          </svg>
-          <div class="recipe__quantity">0.5</div>
-          <div class="recipe__description">
-            <span class="recipe__unit">cup</span>
-            ricotta cheese
-          </div>
-        </li>
-      </ul>
+        `
+    }).join('')}
+       </ul>
     </div>
 
     <div class="recipe__directions">
