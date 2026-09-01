7f42eb2cd80b6f5e34a9c0947e533c250e746fed
omidsdgi <omid69sdgi@gmail.com>
2026-09-01 14:28:20 +0330

    feat(spinner): implement initial spinner

--- src/js/controller.js
+++ src/js/controller.js
@@ -15,6 +15,18 @@ const timeout= function(s){
 // https://forkify-api.jonas.io
 /////////////////////////////////////////////////
 
+const renderSpinner = function(parentEl){
+  const markup = `
+  <div class="spinner">
+          <svg>
+            <use href="${icons}#icon-loader"></use>
+          </svg>
+        </div> -->
+  `
+  parentEl.innerHTML = '';
+  parentEl.insertAdjacentHTML('afterbegin', markup);
+}
+
 const showRecipe= async function(){
   try {
     // 1) Loading recipe
