8943ecc4f6c0cf403456a9a75af5d8660ca4140a
omidsdgi <omid69sdgi@gmail.com>
2026-09-04 10:17:21 +0330

    refactor: move Address  icon  up

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,5 +1,5 @@
 // import icons from "../img/icons.svg"; //Parcel 1
-import icons from "url:../img/icons.svg"; //Parcel 2
+import icons from "url:../../img/icons.svg"; //Parcel 2
 
 class RecipeView {
   #parentElement= document.querySelector('.recipe');
