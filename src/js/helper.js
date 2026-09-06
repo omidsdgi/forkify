2e17279b55a71182e3ef62549ffb1515c03c5547
omidsdgi <omid69sdgi@gmail.com>
2026-09-06 20:03:07 +0330

    feat(helper): add fetch timeout handling

--- src/js/helper.js
+++ src/js/helper.js
@@ -1,7 +1,17 @@
+import { TIMEOUT_SEC } from '../../omid-copySrc/js/config';
+
+
+const timeout= function(s){
+  return new Promise(function(_,reject){
+    setTimeout(function(){
+      reject(new Error(`Request took too long! Timout after ${s} second`));
+    }, s*1000)
+  });
+}
 
 export const getJson= async function (url) {
   try{
-    const res=await fetch(url);
+    const res=await Promise.race([fetch(url), timeout(.05)]);
     const data = await res.json();
 
     if(!res.ok) throw new Error(`${data.message} ${res.status}`);
