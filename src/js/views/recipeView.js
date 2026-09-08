b5bdd5525e1dd5886a1cbfa9d25c7d326f602082
omidsdgi <omid69sdgi@gmail.com>
2026-09-08 13:30:43 +0330

    feat(controller): implement initial error component

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -16,6 +16,8 @@ class RecipeView {
     this.#parentElement.innerHTML = '';
   }
 
+
+
    renderSpinner =()=>{
    const markup=   `
   <div class="spinner">
@@ -24,11 +26,24 @@ class RecipeView {
           </svg>
         </div> -->        
   `
-    this.#parentElement.innerHTML = '';
+    this.#clear()
     this.#parentElement.insertAdjacentHTML('afterbegin', markup);
   }
 
-
+ renderError(message){
+    const markup=  `
+    <div class="error">
+            <div>
+              <svg>
+                <use href="{icons}#icon-alert-triangle"></use>
+              </svg>
+            </div>
+            <p>${message}</p>
+          </div>
+    `
+   this.#clear()
+   this.#parentElement.insertAdjacentHTML('afterbegin', markup);
+ }
 
   addHandlerRender(handler){
   ['hashchange','load'].forEach(ev=>window.addEventListener(ev, handler));
