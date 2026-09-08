8d6ebf5f14e9bf61a599de5bbc29cad9ad6a0eb1
omidsdgi <omid69sdgi@gmail.com>
2026-09-08 13:36:14 +0330

    feat(controller): initialize error handling

--- src/js/controller.js
+++ src/js/controller.js
@@ -28,7 +28,7 @@ const controlRecipes= async function(){
     // Rendering recipe
     recipeView.render(model.state.recipe);
   }catch (error) {
-    console.log(error);
+    recipeView.renderError(`${error} 🎇🎇🎇🎇`);
   }
 }
 controlRecipes().catch(error => console.log(error));
