2cee46bf9b16f556b92724c0a0056788a45ab04f
omidsdgi <omid69sdgi@gmail.com>
2026-09-09 14:04:49 +0330

    feat(search-view): handle clear input

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -2,9 +2,14 @@ class SearchView {
   #parentEl=document.querySelector('.search')
 
   getQuery(){
-    return this.#parentEl.querySelector('.search__field').value
+    const query= this.#parentEl.querySelector('.search__field').value
+    this.#clearInput()
+    return query
   }
 
+  #clearInput(){
+    this.#parentEl.querySelector('.search__field').value = ''
+  }
   addHandlerSearch(handler){
     this.#parentEl.addEventListener('submit', function(e)  {
       e.preventDefault()
