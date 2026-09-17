f1c2f5fc1868df90b3332a157228a67f37600c2f
omidsdgi <omid69sdgi@gmail.com>
2026-09-17 12:03:06 +0330

    feat(result-view): render search result on UI

--- src/js/views/resultsView.js
+++ src/js/views/resultsView.js
@@ -1,28 +1,32 @@
 import View from './view';
 
 class ResultsView extends View {
-  _parentElement= document.querySelector('.results');
+  _parentElement = document.querySelector('.results');
 
-  _generateMarkup(){
+  _generateMarkup() {
+    console.log(this._data);
+    return this._data.map(this._generateMarkupPreview).join('');
+  }
+
+  _generateMarkupPreview(result) {
     return `
-    <li class="preview">
-            <a class="preview__link preview__link--active" href="#23456">
-              <figure class="preview__fig">
-                <img src="src/img/test-1.jpg" alt="Test" />
-              </figure>
-              <div class="preview__data">
-                <h4 class="preview__title">Pasta with Tomato Cream ...</h4>
-                <p class="preview__publisher">The Pioneer Woman</p>
-                <div class="preview__user-generated">
-                  <svg>
-                    <use href="src/img/icons.svg#icon-user"></use>
-                  </svg>
-                </div>
-              </div>
-            </a>
-          </li>
+     <li class="preview">
+        <a class="preview__link" href="#${result.id}">
+          <figure class="preview__fig">
+            <img src="${result.image_url}" alt="${result.title}" />
+          </figure>
+          <div class="preview__data">
+            <h4 class="preview__title">${result.title}</h4>
+            <p class="preview__publisher">${result.publisher}</p>
+            <div class="preview__user-generated">
+              <svg>
+                <use href="src/img/icons.svg#icon-user"></use>
+              </svg>
+            </div>
+          </div>
+        </a>
+      </li>
     `
   }
 }
-
 export default new ResultsView();
