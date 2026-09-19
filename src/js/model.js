78cfff018c4c13355b554fbda931bfdb8abeee3d
omidsdgi <omid69sdgi@gmail.com>
2026-09-19 20:57:46 +0330

    feat(config): add constant variable

--- src/js/model.js
+++ src/js/model.js
@@ -7,7 +7,7 @@ export const state={
   search: {
     query: '',
     results: [],
-    resultsPerPage: 10
+    resultsPerPage: RES_PER_PAG
   }
 }
 
@@ -59,6 +59,6 @@ export const loadSearchResults = async function(query){
 export const getSearchResultsPage =  function(page){
   const start= (page-1) * state.search.resultsPerPage
   const end= page * state.search.resultsPerPage
-  
+
   return state.search.results.slice(start, end);
 }
