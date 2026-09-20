851d1582a663cb9388a52992a023fbd06dd45594
omidsdgi <omid69sdgi@gmail.com>
2026-09-20 11:57:02 +0330

    feat(pagination-view): display button for last page

--- src/js/views/paginationView.js
+++ src/js/views/paginationView.js
@@ -7,17 +7,25 @@ class PaginationView extends View{
   _generateMarkup(){
     const numPages=Math.ceil(this._data.results.length / this._data.resultsPerPage);
     console.log(numPages);
+    const currPage=this._data.page;
     // Page 1, and there are other pages
-    if (this._data.page ===1 && numPages>1){
+    if (currPage ===1 && numPages>1){
       return `page 1, others`
     }
 
     //Last page
-    if(this._data.page === numPages && numPages >1){
-      return `last page`
+    if(currPage === numPages && numPages >1){
+      return `
+       <button class="btn--inline pagination__btn--prev">
+            <svg class="search__icon">
+              <use href="${icons}#icon-arrow-left"></use>
+            </svg>
+            <span>Page ${currPage-1}</span>
+          </button>
+      `
     }
     //other page
-    if( this._data.page < numPages ){
+    if( currPage < numPages ){
     // Page 1, and there are No other pages
       return `other page`
     }
