ddebf300e897e79b88f528857db8f8839d82e743
omidsdgi <omid69sdgi@gmail.com>
2026-09-20 11:23:22 +0330

    feat(pagination-view): implement initial pagination view

---
+++ src/js/views/paginationView.js
@@ -0,0 +1,10 @@
+import View from './view';
+import icons from 'url:../../img/icons.svg';
+
+class PaginationView extends View{
+  _parentElement= document.querySelector('.pagination');
+
+  _generateMarkup(){
+
+  }
+}
